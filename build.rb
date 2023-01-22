ddd72aed4a8d4fdc20effa6a045b869103a3ee8a
Rick Carlino <rick.carlino@gmail.com>
2023-01-22 10:20:10 -0600

    Update `build.rb` file

--- build.rb
+++ build.rb
@@ -1,24 +1,4 @@
-HEADER = \
-"""
-## Openscad Models
-
-Shared models in openscad for sharing, learning, blah, blah.
-
-## How To Build
-
-Render all *.scad files and update the README in one swoop!
-
-Robert check this out: [docs](https://gist.github.com/RickCarlino/ca1da717c5353c2a29228dd3ff13643f)
-
-```
-ruby build.rb
-```
-
-# Notes
-
-Files that `_start_with_an_underscore` won't be rendered.
-"""
-
+HEADER = File.read("./header.md")
 RENDERING = \
 """
 # %s.scad
---
+++ header.md
@@ -0,0 +1,17 @@
+## Openscad Models
+
+Shared models in openscad for sharing, learning, blah, blah.
+
+## How To Build
+
+Render all *.scad files and update the README in one swoop!
+
+Robert check this out: [docs](https://gist.github.com/RickCarlino/ca1da717c5353c2a29228dd3ff13643f)
+
+```
+ruby build.rb
+```
+
+# Notes
+
+Files that `_start_with_an_underscore` won't be rendered.
